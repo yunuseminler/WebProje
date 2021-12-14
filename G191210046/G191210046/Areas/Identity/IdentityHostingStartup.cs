cfb5053aef63dbd9aa041ae78382507e5f4f1f2b
yunuseminler <yunus.eminler@gmail.com>
2021-12-14 15:34:09 +0300

    Roller verildi

---
+++ G191210046/G191210046/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using G191210046.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(G191210046.Areas.Identity.IdentityHostingStartup))]
+namespace G191210046.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ G191210046/G191210046/Data/DbInitializer.cs
@@ -0,0 +1,62 @@
+﻿using G191210046.Data;
+using G191210046.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Linq;
+
+namespace G191210046.Data
+{
+    public class DbInitializer : IDbInitializer
+    {
+
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public DbInitializer(ApplicationDbContext db, 
+            UserManager<IdentityUser> userManager, 
+            RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+
+        public async void Initialize()
+        {
+            try
+            {
+                if(_db.Database.GetPendingMigrations().Count()>0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+
+            if (_db.Roles.Any(r => r.Name == "Admin")) return;
+
+            _roleManager.CreateAsync(new IdentityRole("Admin")).GetAwaiter().GetResult();
+            _roleManager.CreateAsync(new IdentityRole("User")).GetAwaiter().GetResult();
+
+            _userManager.CreateAsync(new ApplicationUser
+            {
+                UserName = "admin@gmail.com",
+                Email = "admin@gmail.com",
+                Ad = "Yunus Emre",
+                Soyad="Eminler",
+                EmailConfirmed = true,
+                PhoneNumber = "1112223333"
+            }, "Admin123*").GetAwaiter().GetResult();
+
+
+            _userManager.AddToRoleAsync(_db.Users.FirstOrDefaultAsync(u => u.Email == "admin@gmail.com").GetAwaiter().GetResult(), "Admin").GetAwaiter().GetResult();
+
+        }
+    }
+}
