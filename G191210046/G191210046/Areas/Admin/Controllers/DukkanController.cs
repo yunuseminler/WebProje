3508da2a60b1d064027e8e20c84c8b24197f6707
yunuseminler <yunus.eminler@gmail.com>
2021-12-17 21:25:14 +0300

    Sayfalar düzenlendi

---
+++ G191210046/G191210046/Areas/Admin/Controllers/DukkanController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using G191210046.Data;
+using G191210046.Models;
+
+namespace G191210046.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DukkanController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DukkanController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Dukkan
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Dukkan.ToListAsync());
+        }
+
+        // GET: Admin/Dukkan/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dukkan = await _context.Dukkan
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (dukkan == null)
+            {
+                return NotFound();
+            }
+
+            return View(dukkan);
+        }
+
+        // GET: Admin/Dukkan/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Dukkan/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Ad,Adres,TelNo,Aciklama")] Dukkan dukkan)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(dukkan);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(dukkan);
+        }
+
+        // GET: Admin/Dukkan/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dukkan = await _context.Dukkan.FindAsync(id);
+            if (dukkan == null)
+            {
+                return NotFound();
+            }
+            return View(dukkan);
+        }
+
+        // POST: Admin/Dukkan/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Ad,Adres,TelNo,Aciklama")] Dukkan dukkan)
+        {
+            if (id != dukkan.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(dukkan);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DukkanExists(dukkan.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(dukkan);
+        }
+
+        // GET: Admin/Dukkan/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dukkan = await _context.Dukkan
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (dukkan == null)
+            {
+                return NotFound();
+            }
+
+            return View(dukkan);
+        }
+
+        // POST: Admin/Dukkan/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var dukkan = await _context.Dukkan.FindAsync(id);
+            _context.Dukkan.Remove(dukkan);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DukkanExists(int id)
+        {
+            return _context.Dukkan.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ G191210046/G191210046/Controllers/SiparisController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using G191210046.Data;
+using G191210046.Models;
+
+namespace G191210046.Controllers
+{
+    public class SiparisController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SiparisController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Siparis
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Siparis.Include(s => s.ApplicationUser);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Siparis/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var siparis = await _context.Siparis
+                .Include(s => s.ApplicationUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (siparis == null)
+            {
+                return NotFound();
+            }
+
+            return View(siparis);
+        }
+
+        // GET: Siparis/Create
+        public IActionResult Create()
+        {
+            ViewData["MusteriId"] = new SelectList(_context.ApplicationUser, "Id", "Id");
+            return View();
+        }
+
+        // POST: Siparis/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,MusteriId,ToplamUcret,SiparisTarihi,SiparisDurumu,SiparisKodu,Aciklama")] Siparis siparis)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(siparis);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MusteriId"] = new SelectList(_context.ApplicationUser, "Id", "Id", siparis.MusteriId);
+            return View(siparis);
+        }
+
+        // GET: Siparis/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var siparis = await _context.Siparis.FindAsync(id);
+            if (siparis == null)
+            {
+                return NotFound();
+            }
+            ViewData["MusteriId"] = new SelectList(_context.ApplicationUser, "Id", "Id", siparis.MusteriId);
+            return View(siparis);
+        }
+
+        // POST: Siparis/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,MusteriId,ToplamUcret,SiparisTarihi,SiparisDurumu,SiparisKodu,Aciklama")] Siparis siparis)
+        {
+            if (id != siparis.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(siparis);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SiparisExists(siparis.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MusteriId"] = new SelectList(_context.ApplicationUser, "Id", "Id", siparis.MusteriId);
+            return View(siparis);
+        }
+
+        // GET: Siparis/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var siparis = await _context.Siparis
+                .Include(s => s.ApplicationUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (siparis == null)
+            {
+                return NotFound();
+            }
+
+            return View(siparis);
+        }
+
+        // POST: Siparis/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var siparis = await _context.Siparis.FindAsync(id);
+            _context.Siparis.Remove(siparis);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SiparisExists(int id)
+        {
+            return _context.Siparis.Any(e => e.Id == id);
+        }
+    }
+}
